4e6f864cc22ed2505f5d45cbd0920c5aa551c785
kimhi <kimhi@asianaidt.com>
2022-08-23 09:31:07 +0900

    setting

---
+++ src/main/java/com/example/Asiana_lab/AsianaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.Asiana_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsianaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsianaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Asiana_lab/AsianaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Asiana_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsianaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
