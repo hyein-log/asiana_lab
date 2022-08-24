0a9244afc17add5be3c047b5fb519d8b5c19f625
leehj3 <leehj3@asiana.com>
2022-08-24 18:53:19 +0900

    현정 reservation

--- src/main/java/com/example/Asiana_lab/controller/ReservationController.java
+++ src/main/java/com/example/Asiana_lab/controller/ReservationController.java
@@ -4,6 +4,7 @@ import com.example.Asiana_lab.model.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,9 @@ public class ReservationController {
     public String reservationMain(Model model){
         model.addAttribute("flights",reservationService.getFlightList());
         return "/reservation/main";
+    }
+    @RequestMapping("/reservation_read/{flight_no}")
+    public String openReservationDetail(@PathVariable("flight_no") int flight_no,Model model){
+        return "reservationDetail";
     }
 }
