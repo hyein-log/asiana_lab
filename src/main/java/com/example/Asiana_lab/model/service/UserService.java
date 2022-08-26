dd5e28b6a855c08ba021d8c89f42b4973302ea74
kimhi <kimhi@asianaidt.com>
2022-08-26 11:08:34 +0900

    혜인 - 이메일, 비번 정보수정 가능

--- src/main/java/com/example/Asiana_lab/model/service/UserService.java
+++ src/main/java/com/example/Asiana_lab/model/service/UserService.java
@@ -5,33 +5,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
-public interface UserService  {
+public interface UserService {
     //아이디 중복 체크
     int idDuplicateCheck(String userid);
+
     //이메일 중복 체크
     int emailDuplicateCheck(String email);
+
     //회원가입
     void join(User user) throws Exception;
+
     //로그인
     String login(HttpServletRequest request, @RequestParam String userid, @RequestParam String password) throws Exception;
 
     void user_delete(int user_no);
 
     //회원정보 수정
-    void changeId(String password) throws Exception;
+    void changeId(User user) throws Exception;
+
     //비밀번호 재설정
-    void changePw(String userid) throws Exception;
+    void changePw(User user) throws Exception;
 
     //아이디 찾기
     String findId(String email) throws Exception;
 
     //회원 탈퇴
     String signOut(HttpServletRequest request, String userid) throws Exception;
+
     //아이디로 회원 불러오기
     User selectOneById(int user_no) throws Exception;
 
     User selectIdByUserid(String userid);
+
     //회원 번호로 아이디 불러오기
     String findIdByNo(int user_no) throws Exception;
 
+    User selectOneByEmail(String email);
+
+    void updatePw(User user);
+
+    void updateEmail(User user);
 }
